eb696cf369c9351e7653e6caa141479f7ede2ffe
nathan <maerna1@gmail.com>
2021-02-05 20:09:24 +0100

    read excel file

--- sources/server/main.c
+++ sources/server/main.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "libxl.h"
+#include <string.h>
+void readData();
 
-int main () {
-    xls_error_t error = LIBXLS_OK;
-    xlsWorkBook *wb = xls_open_file("/path/to/finances.xls", "UTF-8", &error);
-    if (wb == NULL) {
-        printf("Error reading file: %s\n", xls_getError(error));
-        exit(1);
-    }
-    for (int i=0; i<wb->sheets.count; i++) { // sheets
-        xlsWorkSheet *work_sheet = xls_getWorkSheet(work_book, i);
-        error = xls_parseWorkSheet(work_sheet);
-        for (int j=0; j<=work_sheet->rows.lastrow; j++) { // rows
-            xlsRow *row = xls_row(work_sheet, j);
-            for (int k=0; k<=work_sheet->rows.lastcol; k++) { // columns
-                xlsCell *cell = &row->cells.cell[k];
-                // do something with cell
-                if (cell->id == XLS_RECORD_BLANK) {
-                    // do something with a blank cell
-                } else if (cell->id == XLS_RECORD_NUMBER) {
-                   // use cell->d, a double-precision number
-                } else if (cell->id == XLS_RECORD_FORMULA) {
-                    if (strcmp(cell->str, "bool") == 0) {
-                        // its boolean, and test cell->d > 0.0 for true
-                    } else if (strcmp(cell->str, "error") == 0) {
-                        // formula is in error
-                    } else {
-                        // cell->str is valid as the result of a string formula.
-                    }
-                } else if (cell->str != NULL) {
-                    // cell->str contains a string value
+typedef struct Data {
+  double weight;
+  double length;
+  double height;
+  double width ;
+  char emailRecipient[30] ;
+  char addressRecipient[100];
+  uint8_t delay ;
+
+} Data ;
+
+int main(void)
+{
+    readData();
+    return 0;
+}
+
+
+void readData(){
+    Data *datas = malloc(sizeof(Data) * 2);
+    BookHandle book = xlCreateBook();
+    if(book)
+    {
+        if(xlBookLoad(book, "example.xls"))
+        {
+            SheetHandle sheet = xlBookGetSheet(book, 0);
+            if(sheet)
+            {
+                for (size_t lin = 1; lin <= 2; lin++) {
+                    datas[lin-1].weight = xlSheetReadNum(sheet, lin, 0, 0);
+                    datas[lin-1].length = xlSheetReadNum(sheet, lin, 1, 0);
+                    datas[lin-1].height = xlSheetReadNum(sheet, lin, 2, 0);
+                    datas[lin-1].width = xlSheetReadNum(sheet, lin, 3, 0);
+                    strcpy(datas[lin-1].emailRecipient, xlSheetReadStr(sheet, lin, 4, 0));
+                    strcpy(datas[lin-1].addressRecipient, xlSheetReadStr(sheet, lin, 5, 0));
+                    datas[lin-1].delay = xlSheetReadNum(sheet, lin, 6, 0);
                 }
             }
         }
-        xls_close_WS(work_sheet);
+
+        xlBookRelease(book);
     }
-    xls_close_WB(wb);
+    printf("%s\n", datas[1].emailRecipient);
+    free(datas);
 }
